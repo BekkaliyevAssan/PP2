4d48e308971eeba55f39e0b0cb620c0ca39604ff
BekkaliyevAssan <47390497+BekkaliyevAssan@users.noreply.github.com>
2019-02-13 19:44:50 +0600

    Update Program.cs

--- Week2/Task1/Task1/Program.cs
+++ Week2/Task1/Task1/Program.cs
@@ -8,16 +8,16 @@ namespace Directories
         static void Main(string[] args)
         {
             //string text = Console.ReadLine(); IN CASE, IF I SHOULD GET DATA FROM CONSOLE
-            string text = System.IO.File.ReadAllText(@"C:\Users\Fora\source\repos\Week2\TestInput.txt");
-            System.IO.File.WriteAllText(@"C:\Users\Fora\source\repos\Week2\TestOutput.txt", text);
-            System.Console.WriteLine("Content of the Test.txt is  =  {0}", text);
-            char[] ch = text.ToCharArray();
-            Array.Reverse(ch);
-            string reverse = new string(ch);
+            string text = System.IO.File.ReadAllText(@"C:\Users\Fora\source\repos\Week2\TestInput.txt");//This method reads all of the info from the txt
+            System.IO.File.WriteAllText(@"C:\Users\Fora\source\repos\Week2\TestOutput.txt", text);//Comes with the answer to the Output text
+            System.Console.WriteLine("Content of the Test.txt is  =  {0}", text);//This extra row shows content of the input
+            char[] ch = text.ToCharArray();//Converts the word into the chars and put it in the array
+            Array.Reverse(ch);//Reverse
+            string reverse = new string(ch);//Making up a new string with reversed chars
             if (reverse == text)
-                Console.WriteLine("True");
+                Console.WriteLine("True");//Output on console
             else
-                Console.WriteLine("False");
+                Console.WriteLine("False");//Output on console
 
         }
     }
