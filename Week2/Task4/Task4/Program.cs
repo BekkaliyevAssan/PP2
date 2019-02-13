3ff1a1e83317859001907dc62ac41c0e9db75738
BekkaliyevAssan <47390497+BekkaliyevAssan@users.noreply.github.com>
2019-02-13 20:29:49 +0600

    Update Program.cs

--- Week2/Task4/Task4/Program.cs
+++ Week2/Task4/Task4/Program.cs
@@ -8,14 +8,14 @@ namespace Task4
     {
         static void Main(string[] args)
         {
-            string folderName = @"C:\Users\Fora\source\repos\Week2";
-            string path1String = System.IO.Path.Combine(folderName, "path1");
-            System.IO.Directory.CreateDirectory(path1String);
+            string folderName = @"C:\Users\Fora\source\repos\Week2";//showing full name of the folder
+            string path1String = System.IO.Path.Combine(folderName, "path1");//convert the name of the folder into the string 
+            System.IO.Directory.CreateDirectory(path1String);//creating folder for the new file
 
 
-            string fileName = "Original.txt";
-            path1String = System.IO.Path.Combine(path1String, fileName);
-            Console.WriteLine("Path to my new file is {0}\n", path1String);
+            string fileName = "Original.txt";//creating name of the file
+            path1String = System.IO.Path.Combine(path1String, fileName);//SOME STUFF FORM THE FORUM
+            Console.WriteLine("Path to my new file is {0}\n", path1String);//showing new full name of the file
 
             // Just for fun)))
 
@@ -24,29 +24,31 @@ namespace Task4
 
             //
 
-            using (System.IO.FileStream fs = System.IO.File.Create(path1String))
+            using (System.IO.FileStream fs = System.IO.File.Create(path1String)) //method USING to write something into txt file
             {
                 for (int i = 0; i < bArray.Length; i++)
                 {
-                    fs.WriteByte(bArray[i]);
+                    fs.WriteByte(bArray[i]);//using UTF8 coding
                 }
             }
-            string path2String = System.IO.Path.Combine(folderName, "path2");
-            System.IO.Directory.CreateDirectory(path2String);
+            string path2String = System.IO.Path.Combine(folderName, "path2");//convert the name of the folder into the string 
+            System.IO.Directory.CreateDirectory(path2String);//creating folder for the new file
 
-            string sourcePath = @"C:\Users\Fora\source\repos\Week2\path1";
-            string targetPath = @"C:\Users\Fora\source\repos\Week2\path2";
+            string sourcePath = @"C:\Users\Fora\source\repos\Week2\path1";//full name of the initial folder
+            string targetPath = @"C:\Users\Fora\source\repos\Week2\path2";//full name of the secondary folder
 
-            string sourceFile = System.IO.Path.Combine(sourcePath, fileName);
-            string destFile = System.IO.Path.Combine(targetPath, fileName);
+            string sourceFile = System.IO.Path.Combine(sourcePath, fileName);//full name of the initial file
+            string destFile = System.IO.Path.Combine(targetPath, fileName);//full name of the secondary file
 
-            System.IO.File.Copy(sourceFile, destFile, true);
+            System.IO.File.Copy(sourceFile, destFile, true);//method that copies file from one folder to another
 
             // Section to delete the intial one
 
-            if (System.IO.File.Exists(@"C:\Users\Fora\source\repos\Week2\path1\Original.txt"))
+            
+            
+            if (System.IO.File.Exists(@"C:\Users\Fora\source\repos\Week2\path1\Original.txt"))//cheking for the existance of such file
             {
-                System.IO.File.Delete(@"C:\Users\Fora\source\repos\Week2\path1\Original.txt");
+                System.IO.File.Delete(@"C:\Users\Fora\source\repos\Week2\path1\Original.txt");//if yes, then delete it
                 Console.WriteLine("Hello world"); // this line was just for test))
             }
 
