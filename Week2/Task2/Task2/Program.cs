49fd5c4faf28e81af904ab5bda0606437b173699
BekkaliyevAssan <47390497+BekkaliyevAssan@users.noreply.github.com>
2019-02-13 19:51:25 +0600

    Update Program.cs

--- Week2/Task2/Task2/Program.cs
+++ Week2/Task2/Task2/Program.cs
@@ -7,15 +7,15 @@ namespace Week2
     {
         static void Main(string[] args)
         {
-            string[] lines = Console.ReadLine().Split();
-            int[] nums = new int[lines.Length];
+            string[] lines = Console.ReadLine().Split();//divide numbers as a chars IF THE USER WRITE ALL NUMBERS IN THE CMD
+            int[] nums = new int[lines.Length];//array for numbers in the further
 
 
             for (int i = 0; i < lines.Length; i++)
-                nums[i] = Convert.ToInt32(lines[i]);
+                nums[i] = Convert.ToInt32(lines[i]);//converting every char into integer
 
 
-            int[] ans = new int[nums.Length];
+            int[] ans = new int[nums.Length];//putting numbers into new arr
             int prime = 0;
             for (int i = 0; i < nums.Length; i++)
             {
@@ -30,27 +30,29 @@ namespace Week2
                     ans[prime] = nums[i];
                     prime++;
                 }
-            }
+            }//simple algorithm to find out prime numbers and its amount
 
-            string[] ans_string = new string[prime];
+            string[] ans_string = new string[prime];//new array for answer as a string
             for (int i = 0; i < prime; i++)
             {
-                ans_string[i] = Convert.ToString(ans[i]);
+                ans_string[i] = Convert.ToString(ans[i]);//putting numbers as a string into string array
             }
 
-            System.IO.File.WriteAllLines(@"C:\Users\Fora\source\repos\Week2\Task2\input.txt", lines);
-            System.Console.Write("Content of the input.txt is ");
+            System.IO.File.WriteAllLines(@"C:\Users\Fora\source\repos\Week2\Task2\input.txt", lines);//cout >> all numbers 
+            System.Console.Write("Content of the input.txt is ");//shows the content of the input
             foreach (string line in lines)
             {
-                Console.Write(line + " ");
+                Console.Write(line + " ");//leaves free space between chars
             }
-            Console.WriteLine();
+            Console.WriteLine();//new line
 
-            System.IO.File.WriteAllLines(@"C:\Users\Fora\source\repos\Week2\Task2\output.txt", ans_string);
-            System.Console.Write("Content of the input.txt is ");
+            
+            
+            System.IO.File.WriteAllLines(@"C:\Users\Fora\source\repos\Week2\Task2\output.txt", ans_string);//cout >> the final answer
+            System.Console.Write("Content of the input.txt is ");//shows the content of the output
             foreach (string anss in ans_string)
             {
-                Console.Write(anss + " ");
+                Console.Write(anss + " ");//free space
             }
 
         }
